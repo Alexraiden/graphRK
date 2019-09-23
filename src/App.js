d941d2642a38390cc3e79540b932e0d3e61da39f
Alexraiden <al.carrillo@laposte.net>
2019-09-23 20:00:07 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { graphql, QueryRenderer } from 'react-relay'
+import './App.css'
+import environment from './Environment'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query AppQuery {
+            characters(page: 2, filter: { name: "rick" }) {
+              info {
+                count
+              }
+              results {
+                name
+              }
+            }
+            character(id: 1) {
+              id
+            }
+          }
+          `}
+        variables={{
+          pageId: '1'
+        }}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>
+          }
+          if (!props) {
+            return <div>Loading...</div>
+          }
+          return <div>User ID: {props}</div>
+        }}
+      />
+    )
+  }
 }
 
-export default App;
+export default App
